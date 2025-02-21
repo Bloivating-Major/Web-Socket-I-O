9f6167b0c6a5a5e13c5b28b1447ccca1de58d199
Sambhav <sambhav12wakhariya@gmail.com>
2025-02-21 12:20:07 +0530

    Checked In : Added Http Server

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const app = require('./src/app');
 
 //  npm socket.io 
-const server = require('http').createServer();
+const server = require('http').createServer(app);
 const io = require('socket.io')(server);
 io.on('connection', socket => {
     console.log("User Connected");
